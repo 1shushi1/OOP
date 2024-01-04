7f7686b8e5624c0d4df96c3b883d99a67e4e873a
1shushi1 <lytvynyuk.oleksiy1@gmail.com>
2024-01-04 16:35:50 +0100

    1shushi1

--- src/multithreading/synchonized/waifNotify/barberShop/Main.java
+++ src/multithreading/synchonized/waifNotify/barberShop/Main.java
@@ -9,9 +9,13 @@ public class Main {
     public static void main(String[] args) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File("barberShop.txt")))){
 
-            BarberShop barberShop = new BarberShop()
+            BarberShop barberShop = new BarberShop(2, 5, writer);
 
-            ClientGenerator clientGenerator1 = new ClientGenerator()
+            ClientGenerator clientGenerator1 = new ClientGenerator(barberShop, 1, 4, 1, 3);
+            ClientGenerator clientGenerator2 = new ClientGenerator(barberShop, 1, 3, 1, 3);
+            ClientGenerator clientGenerator3 = new ClientGenerator(barberShop, 1, 2, 1, 3);
+
+
         } catch (IOException e){
         }
     }
