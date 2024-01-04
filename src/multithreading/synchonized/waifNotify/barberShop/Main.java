5b3b628844d22ec23042690a93964e1c279aac73
1shushi1 <lytvynyuk.oleksiy1@gmail.com>
2024-01-04 19:56:17 +0100

    main

--- src/multithreading/synchonized/waifNotify/barberShop/Main.java
+++ src/multithreading/synchonized/waifNotify/barberShop/Main.java
@@ -15,8 +15,32 @@ public class Main {
             ClientGenerator clientGenerator2 = new ClientGenerator(barberShop, 1, 3, 1, 3);
             ClientGenerator clientGenerator3 = new ClientGenerator(barberShop, 1, 2, 1, 3);
 
+            BarberShop.Barber barber1 = barberShop.new Barber(1);
+            BarberShop.Barber barber2 = barberShop.new Barber(2);
 
-        } catch (IOException e){
+            BarberShop.QueueMonitoring queueMonitoring = barberShop.new QueueMonitoring();
+
+            barber1.setDaemon(true);
+            barber2.setDaemon(true);
+
+            clientGenerator1.setDaemon(true);
+            clientGenerator2.setDaemon(true);
+            clientGenerator3.setDaemon(true);
+
+            queueMonitoring.setDaemon(true);
+
+            barber1.start();
+            barber2.start();
+
+            clientGenerator1.start();
+            clientGenerator2.start();
+            clientGenerator3.start();
+
+            queueMonitoring.start();
+
+            Thread.sleep(40000);
+        } catch (IOException | InterruptedException e){
+            e.printStackTrace();
         }
     }
 }
